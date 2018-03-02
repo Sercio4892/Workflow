b8ec9fc5f6cb85ac18aedf98d3d8f39d35a37a18
Sercio4892 <sercan.k@outlook.fr>
2018-03-02 23:04:44 +0100

    Initial commit

--- init.sh
+++ init.sh
@@ -27,7 +27,7 @@ curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 cd ~/
-git clone 
+git clone https:// 
 cp /Workflow/.vimrc ~/
 cp /Workflow/.tmux.conf ~/
 
